31841bf0d933e4e515f3120a2c78ec61329ee60a
dzhemriza <kaksibe@gmail.com>
2019-10-24 17:00:17 +0100

    Update random-data.ps1

--- windows/random-data.ps1
+++ windows/random-data.ps1
@@ -8,11 +8,11 @@ $path = "c:\tmp\remove-me-mate-this-is-a-test";
 New-Item -ItemType Directory -Force -Path $path;
 
 $count | ForEach-Object {
-$fileName = "$path\random_$prefix-$_.r";
-echo "File - $fileName";
-$out = new-object byte[] 1048576;
-(new-object Random).NextBytes($out);
-[IO.File]::WriteAllBytes($fileName, $out);
+    $fileName = "$path\random_$prefix-$_.r";
+    echo "File - $fileName";
+    $out = new-object byte[] 1048576;
+    (new-object Random).NextBytes($out);
+    [IO.File]::WriteAllBytes($fileName, $out);
 }
 
 echo "My first PS script...done.";
